01fd1b6b5ccfbaf2a0aad53ac8b98ce0fd1d2a05
omar068 <omar6688.oc@gmail.com>
2023-01-05 02:14:34 -0500

    Modify build file

--- build.sh
+++ build.sh
@@ -2,15 +2,8 @@
 # exit on error
 set -o errexit
 
-#poetry install
-sudo apt install python3-dev
-sudo apt install libpq-dev
-sudo apt install python3-pip
-sudo apt install python3-venv
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-pip install wheel
+poetry install
+sudo apt install python3-dev libpq-dev
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
