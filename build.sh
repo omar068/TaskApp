40669e66b5d99e7436e35a7b14fe4a2f9252616f
omar068 <omar6688.oc@gmail.com>
2023-01-06 00:50:53 -0500

    Añadiendo cositas con la intención de arreglar la base de datos

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+#poetry install
+sudo apt install libpq-dev python3-dev
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
